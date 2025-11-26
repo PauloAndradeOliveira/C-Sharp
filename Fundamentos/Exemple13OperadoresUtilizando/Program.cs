b65f8d2254c2d713d01cb9c909a427ba7f29988d
PauloAndradeOliveira <pcoliveiraandrade@outlook.com>
2025-11-26 13:47:50 -0300

    Operadores Matematicos

---
+++ Fundamentos/Exemple13OperadoresUtilizando/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("### Utilizando operadores ###\n");
+
+#region Operadores aritimeticos
+Console.WriteLine("Soma");
+var a = 2;
+var b = 3;
+var soma = a + b;
+Console.WriteLine($"A soma de {a} + {b} é: {soma}");
+
+Console.WriteLine("\nSubtração ");
+var c = 2;
+var d = 3;
+var subtracao = c + d;
+Console.WriteLine($"A subtração de {c} + {d} é: {subtracao}");
+
+Console.WriteLine("\nMultiplicação");
+var e = 10;
+var f = 20;
+var multipliacao = e + f;
+Console.WriteLine($"A multiplicação de {e} + {f} é : {multipliacao}");
+
+Console.WriteLine("\nDivisão");
+var g = 2;
+var h = 3;
+var divisao = g / h;
+Console.WriteLine($"A Divisão de {g} + {h} é: {divisao}");
+#endregion
+
+Console.ReadKey();
