df2aa68f1d34ebb2b5946b1f80b83e30ea806ca5
PauloAndradeOliveira <pcoliveiraandrade@outlook.com>
2025-11-26 20:46:47 -0300

    Operadores Lógico AND (&&) e Or (||)

---
+++ Fundamentos/Exemple15OperadoresLogicos/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("### Operadores Lógicos ###");
+
+var x = 5;
+var b = 33;
+
+// Ambas as condições verdadeiras
+if (x >= 5 && b >= 32)
+{
+    Console.WriteLine("Todas as condições são verdadeiras.");
+}
+// Pelo menos uma condição verdadeira
+else if (x >= 5 || b > 33)
+{
+    Console.WriteLine("Pelo menos uma condição é verdadeira.");
+}
+else
+{
+    Console.WriteLine("Nenhuma condição é verdadeira.");
+}
+
+Console.ReadKey();
