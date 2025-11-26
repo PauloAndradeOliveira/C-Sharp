8056417103a74de92a46fd6777a64a1decaffc01
PauloAndradeOliveira <pcoliveiraandrade@outlook.com>
2025-11-26 14:12:26 -0300

    Operadores de Comparação

--- Fundamentos/Exemple13OperadoresUtilizando/Program.cs
+++ Fundamentos/Exemple13OperadoresUtilizando/Program.cs
@@ -27,20 +27,54 @@
 #endregion
 
 #region Operadores de Atribuição
-var x = 2;
-x += 3;
-Console.WriteLine($"Utilizando o operador de atribuição ( += ) : {x}");
+//var x = 2;
+//x += 3;
+//Console.WriteLine($"Utilizando o operador de atribuição ( += ) : {x}");
 
-var b = 2;
-b -= 3;
-// Valor fica ( -1 ) pois estamos dizendo que (b -= 3)
-Console.WriteLine($"Utilizando o operador de atribuição ( -= ) : {b}");
+//var b = 2;
+//b -= 3;
+//// Valor fica ( -1 ) pois estamos dizendo que (b -= 3)
+//Console.WriteLine($"Utilizando o operador de atribuição ( -= ) : {b}");
 
-var c = 2;
-c = 3;
-// valor atribuido é 2.
-// Após a atribuição o valor fica 3.
-Console.WriteLine($"Utilizando o operador de atribuição ( = ) : {c}");
+//var c = 2;
+//c = 3;
+//// valor atribuido é 2.
+//// Após a atribuição o valor fica 3.
+//Console.WriteLine($"Utilizando o operador de atribuição ( = ) : {c}");
+#endregion
+
+#region Operador de Comparação 
+Console.WriteLine("\n### Operadores de Comparação ###");
+
+// Operador ==
+Console.WriteLine("\nOperador (==) - Igual");
+int a = 2, b = 3;
+Console.WriteLine($"{a} é igual a {b}? {a == b}");
+
+// Operador !=
+Console.WriteLine("\nOperador (!=) - Diferente");
+int c = 4, d = 5;
+Console.WriteLine($"{c} é diferente de {d}? {c != d}");
+
+// Operador >
+Console.WriteLine("\nOperador (>) - Maior");
+int e = 6;
+Console.WriteLine($"{a} é maior que {e}? {a > e}");
+
+// Operador >=
+Console.WriteLine("\nOperador (>=) - Maior ou Igual");
+int f = 34, g = 20;
+Console.WriteLine($"{f} é maior ou igual a {g}? {f >= g}");
+
+// Operador <
+Console.WriteLine("\nOperador (<) - Menor");
+int h = 10, i = 20;
+Console.WriteLine($"{h} é menor que {i}? {h < i}");
+
+// Operador <=
+Console.WriteLine("\nOperador (<=) - Menor ou Igual");
+int j = 15, k = 15;
+Console.WriteLine($"{j} é menor ou igual a {k}? {j <= k}");
 #endregion
 
 Console.ReadKey();
